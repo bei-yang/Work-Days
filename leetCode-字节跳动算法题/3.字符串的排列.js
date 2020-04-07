03c16e41b31237dab183f85a31b245f84c823ffc
beiyang <979623625@qq.com>
2020-04-07 23:35:47 +0800

    feature:字符串的排列

--- leetCode-字节跳动算法题/3.字符串的排列.js
+++ leetCode-字节跳动算法题/3.字符串的排列.js
@@ -9,4 +9,92 @@ var checkInclusion = function (s1, s2) {
     count[s1[i].charCodeAt() - 97]++
     count[s2[i].charCodeAt() - 97]--
   }
+  if (isAllZero(count)) return true
+  for (let i = n1; i < n2; i++) {
+    count[s2[i].charCodeAt() - 97]--
+    count[s2[i - n1].charCodeAt() - 97]++
+    if (isAllZero(count)) return true
+  }
+  return false
+}
+// 2
+var checkInclusion = function (s1, s2) {
+  let n1 = s1.length, n2 = s2.length
+  if (n1 > n2) return
+  let count = new Array(26).fill(0)
+  let isAllZero = (count) => {
+    return count.every(val => val === 0)
+  }
+  for (let i = 0; i < n1; i++) {
+    count[s1[i].charCodeAt() - 97]++
+    count[s2[i].charCodeAt() - 97]--
+  }
+  if (isAllZero(count)) return true
+  for (let i = n1; i < n2; i++) {
+    count[s2[i].charCodeAt() - 97]--
+    count[s2[i - n1].charCodeAt() - 97]++
+    if (isAllZero(count)) return true
+  }
+  return false
+}
+
+// 3
+var checkInclusion = function (s1, s2) {
+  let n1 = s1.length, n2 = s2.length
+  if (n1 > n2) return false
+  let count = new Array(26).fill(0)
+  let isAllZero = (count) => {
+    return count.every(val => val === 0)
+  }
+  for (let i = 0; i < n1; i++) {
+    count[s1[i].charCodeAt() - 97]++
+    count[s2[i].charCodeAt() - 97]--
+  }
+  if (isAllZero(count)) return true
+  for (let i = n1; i < n2; i++) {
+    count[s2[i].charCodeAt() - 97]--
+    count[s2[i - n1].charCodeAt() - 97]++
+    if (isAllZero(count)) return true
+  }
+  return false
+}
+// 4
+var checkInclusion = function (s1, s2) {
+  let n1 = s1.length, n2 = s2.length
+  if (n1 > n2) return false
+  let count = new Array(26).fill(0)
+  let isAllZero = (count) => {
+    return count.every(val => val === 0)
+  }
+  for (let i = 0; i < n1; i++) {
+    count[s1[i].charCodeAt() - 97]++
+    count[s2[i].charCodeAt() - 97]--
+  }
+  if (isAllZero(count)) return true
+  for (let i = n1; i < n2; i++) {
+    count[s1[i].charCodeAt() - 97]--
+    count[s2[i - n1].charCodeAt() - 97]++
+    if (isAllZero(count)) return true
+  }
+  return false
+}
+// 5
+var checkInclusion = function (s1, s2) {
+  let n1 = s1.length, n2 = s2.length
+  if (n1 > n2) return false
+  let count = new Array(26).fill(0)
+  let isAllZero = (count) => {
+    count.every((item) => item === 0)
+  }
+  for (let i = 0; i < n1; i++) {
+    count[s1[i].charCodeAt() - 97]++
+    count[s2[i].charCodeAt() - 97]--
+  }
+  if (isAllZero(count)) return true
+  for(let j=0;j<n2;j++){
+    count[s1[i].charCodeAt()-97]--
+    count[s2[i-n1.charCodeAt()-97]]++
+    if(isAllZero(count)) return true
+  }
+  return false
 }
