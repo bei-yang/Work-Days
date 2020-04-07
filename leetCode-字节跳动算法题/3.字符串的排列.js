470ff3916d38c4ea1e0ed285816bcc380b270799
beiyang <979623625@qq.com>
2020-04-07 23:11:30 +0800

    feature:字符串排序

--- leetCode-字节跳动算法题/3.字符串的排列.js
+++ leetCode-字节跳动算法题/3.字符串的排列.js
@@ -1,9 +1,12 @@
-var checkInclusion = function(s1, s2) {
-  if(s2.indexOf(s1)!==-1||s2.split('').reverse().join('').indexOf(s1)!==-1){
-      return true
-  }else{
-      return false
+var checkInclusion = function (s1, s2) {
+  let n1 = s1.length, n2 = s2.length
+  if (n1 > n2) return false
+  let count = new Array(26).fill(0)
+  let isAllZero = (count) => {
+    return count.every(val => val === 0)
   }
-};
-
-console.log(checkInclusion('abc','bbbca'))
+  for (let i = 0; i < n1; i++) {
+    count[s1[i].charCodeAt() - 97]++
+    count[s2[i].charCodeAt() - 97]--
+  }
+}
