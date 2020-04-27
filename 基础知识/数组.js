8aa5cbca9dad851aa153fd8b134e60acff79dd5d
beiyang <979623625@qq.com>
2020-04-27 23:34:45 +0800

    1

---
+++ 基础知识/数组.js
@@ -0,0 +1,9 @@
+// var fruits = ["Banana", "Orange", "Apple", "Mango"];
+// fruits.copyWithin(2, 0);
+
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.entries();
+
+
+console.log(["Banana", "Orange", "Apple", "Mango"].entries())
