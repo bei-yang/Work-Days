20db4f5912848d6705af52c2f1e7e4db6af07eed
beiyang <979623625@qq.com>
2020-05-02 08:25:35 +0800

    3

--- 300分钟搞定数据结构与算法/6.动态规划.js
+++ 300分钟搞定数据结构与算法/6.动态规划.js
@@ -93,3 +93,138 @@ function knapSack (capacity, weights, values) {
   }
   return ks[n][capacity]
 }
+
+function knapSack (capacity, weights, values) {
+  const n = values.length
+  const ks = []
+  for (let i = 0; i <= n; i++) {
+    ks[i] = []
+  }
+  for (let i = 0; i <= n; i++) {
+    for (let w = 0; w <= capacity; w++) {
+      if (i === 0 || w === 0) {
+        ks[i][w] = 0
+      } else if (weights[i - 1] <= w) {
+        const a = values[i - 1] + ks[i - 1][w - weights[i - 1]]
+        const b = ks[i - 1][w]
+        ks[i][w] = a > b ? a : b
+      } else {
+        ks[i][w] = ks[i - 1][w]
+      }
+    }
+  }
+  return ks[n][capacity]
+}
+
+
+function knapSack (capacity, weights, values) {
+  const n = values.length
+  const ks = []
+  for (let i = 0; i <= n; i++) {
+    ks[i] = []
+  }
+  for (let i = 0; i <= n; i++) {
+    for (let w = 0; w <= capacity; w++) {
+      if (i === 0 || w === 0) {
+        ks[i][w] = 0
+      } else if (weights[i - 1] <= w) {
+        const a = values[i - 1] + ks[i - 1][w - weights[i - 1]]
+        const b = ks[i - 1][w]
+        ks[i][w] = a > b ? a : b
+      } else {
+        ks[i][w] = ks[i - 1][w]
+      }
+    }
+  }
+  return ks[n][capacity]
+}
+
+function knapSack (capacity, weights, values) {
+  const n = values.length
+  const ks = []
+  for (let i = 0; i <= n; i++) {
+    ks[i] = []
+  }
+  for (let i = 0; i <= n; i++) {
+    for (let w = 0; w <= capacity; w++) {
+      if (i === 0 || w === 0) {
+        ks[i][w] = 0
+      } else if (weights[i - 1] <= w) {
+        const a = values[i - 1] + ks[i - 1][w - weights[i - 1]]
+        const b = ks[i - 1][w]
+        ks[i][w] = a > b ? a : b
+      } else {
+        ks[i][w] = ks[i - 1][w]
+      }
+    }
+  }
+  return ks[n][capacity]
+}
+
+function knapSack (capacity, weights, values) {
+  const n = values.length
+  const ks = []
+  for (let i = 0; i <= n; i++) {
+    ks[i] = []
+  }
+  for (let i = 0; i <= n; i++) {
+    for (let w = 0; w <= capacity; w++) {
+      if (i === 0 || w === 0) {
+        ks[i][w] = 0
+      } else if (weights[i - 1] <= w) {
+        const a = values[i - 1] + ks[i - 1][w - weights[i - 1]]
+        const b = ks[i - 1][w]
+        ks[i][w] = a > b ? a : b
+      } else {
+        ks[i][w] = ks[i - 1][w]
+      }
+    }
+  }
+  return ks[n][capacity]
+}
+
+function knapSack (capacity, weights, values) {
+  const len = values.length
+  const ks = []
+  for (let i = 0; i <= len; i++) {
+    ks[i] = []
+  }
+  for (let m = 0; m <= len; m++) {
+    for (let n = 0; n <= capacity; n++) {
+      if (m === 0 || n === 0) {
+        ks[m][n] = 0
+      } else if (weight[i - 1] <= w) {
+        const a = values[i - 1] + ks[i - 1][w - weights[i - 1]]
+        const b = ks[i - 1][w]
+        ks[i][w] = a > b ? a : b
+      } else {
+        ks[i][w] = ks[i - 1][w]
+      }
+    }
+  }
+  return ks[n][capacity]
+}
+
+
+function knapSack(capacity,weights,values){
+  const len=values.length
+  const ks=[]
+  for(let i=0;i<=len;i++){
+    ks[i]=[]
+  }
+  for(let m=0;m<=len;m++){
+    for(let n=0;n<=capacity;n++){
+      if(m===0||n===0){
+        ks[m][n]=0
+      }else if(weights[m-1]<=n){
+        const a=values[m-1]+ks[m-1][n-weights[m-1]]
+        const b=ks[m-1][n]
+        ks[m][n]=a>b?a:b
+      }else{
+        ks[m][n]=ks[m-1][n]
+      }
+    }
+  }
+  return ks[len][capacity]
+}
+console.log(knapSack(5, [2, 3, 4], [3, 4, 5], 3))
