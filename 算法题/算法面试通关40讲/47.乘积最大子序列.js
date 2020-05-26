f4210979e1a3b3cdbb9d24977c5a0e06be5028be
beiyang <979623625@qq.com>
2020-05-26 23:53:43 +0800

    1

--- 算法题/算法面试通关40讲/47.乘积最大子序列.js
+++ 算法题/算法面试通关40讲/47.乘积最大子序列.js
@@ -1,50 +1,33 @@
-/* 
+// 152 题
 
-*/
-
-// var maxPrduct = function (nums) {
-//   if (!nums.length) {
-//     return null
-//   }
-//   let state = []
-//   let max = nums[0]
-//   for (let i = 0; i < nums.length; i++) {
-//     state[i] = []
-//   }
-//   state[0][0] = nums[0]
-//   state[0][1] = nums[0]
-//   for (let i = 1; i < nums.length; i++) {
-//     state[i][0] = Math.max(state[i - 1][0] * nums[i], nums[i], state[i - 1][1] * nums[i])
-//     state[i][1] = Math.max(state[i - 1][1] * nums[i], nums[i], state[i - 1][0] * nums[i])
-//     if (max < state[i][0]) max = state[i][0]
-//   }
-//   return max
-// }
-
-
+// 暴力法
 var maxProduct = function (nums) {
-  if (!nums.length) return null
-  let [max, curMax, curMin] = [nums[0], nums[0], nums[0]]
+  if (nums.length === 0) return null
+  if (nums.length === 1) return nums[0]
+  let max = Number.MIN_SAFE_INTEGER
+  let mid
+  for (let i = 0; i < nums.length; i++) {
+    mid = nums[i]
+    max = Math.max(max, mid)
+    for (let j = i + 1; j < nums.length; j++) {
+      mid = mid * nums[j]
+      max = Math.max(max, mid)
+    }
+  }
+  return max
+}
+
+// dp
+var maxProduct = function (nums) {
+  let max = nums[0]
+  let min = nums[0]
+  let res = nums[0]
 
   for (let i = 1; i < nums.length; i++) {
-    [curMax, curMin] = [curMax * nums[i], curMin * nums[i]];
-    [curMax, curMin] = [Math.max(curMax, curMin, nums[i]), Math.min(curMax, curMin, nums[i])]
-    if (max < curMax) max = curMax;
-  };
-  return max
-};
-
-var maxProduct = function (nums) {
-  if (!nums.length) return null
-  let [max, curMax, curMin] = [nums[0], nums[0], nums[0]]
-
-  for (let i = 1; i < nums.length; i++) {
-    [curMax, curMin] = [curMax * nums[i], curMin * nums[i]];
-    [curMax, curMin] = [Math.max(curMax, curMin, nums[i]), Math.min(curMax, curMin, nums[i])];
-    if (max < curMax) max = curMax;
-  };
-  return max
-};
-
-
-console.log(maxProduct([2, 3, -2, 4]))
+    let tmp = min
+    min = Math.min(nums[i], Math.min(max * nums[i], min * nums[i]))
+    max = Math.max(nums[i], Math.max(max * nums[i], tmp * nums[i]))
+    res = Math.max(res, max)
+  }
+  return res
+}
