6ebc5e673ef388e7d21f62cf07befa81a56b1d28
beiyang <979623625@qq.com>
2020-05-29 07:29:37 +0800

    1

--- 算法题/算法面试通关40讲/53.岛屿的个数&朋友圈.js
+++ 算法题/算法面试通关40讲/53.岛屿的个数&朋友圈.js
@@ -32,95 +32,6 @@ var numIslands = function (grid) {
 
 var findCircleNum = function (M) {
   let n = M.length
-  if (n === 0) {
-    return 0
-  }
-  let count = n
-  let rank = new Array(n)
-  let find = (p) => {
-    while (p != parent[p]) {
-      parent[p] = parent[parent[p]]
-      p = parent[p]
-    }
-    return p
-  }
-  let union = (p, q) => {
-    let rootP = find(p)
-    let rootQ = find(q)
-    if (rootP === rootQ) {
-      return
-    }
-    if (rank[rootP] > rank[rootQ]) {
-      parant[rootQ] = rootP
-    } else if (rank[rootP] < rank[rootQ]) {
-      parent[rootP] = rootQ
-    } else {
-      parent[rootQ] = rootP
-      rank[rootP]++
-    }
-    count--
-  }
-  let parent = new Array(n)
-  for (let i = 0; i < n; i++) {
-    parent[i] = i
-    rank[i] = 1
-  }
-  for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (M[i][j] === 1) {
-        union(i, j)
-      }
-    }
-  }
-  return count
-}
-
-var findCircleNum = function (M) {
-  let n = M.length
-  if (n === 0) return 0
-  let count = 0
-  let dfs = (i) => {
-    for (let j = 0; j < n; j++) {
-      if (M[i][j] === 1 && !visited[j]) {
-        visited[j] = true
-        dfs(j)
-      }
-    }
-  }
-  let visited = {}
-  for (let i = 0; i < n; i++) {
-    if (!visited[i]) {
-      dfs(i)
-      count++
-    }
-  }
-  return count
-}
-
-var findCircleNum = function (M) {
-  let n = M.length
-  if (n === 0) return 0
-  let count = 0
-  let dfs = (i) => {
-    for (let j = 0; j < n; j++) {
-      if (M[i][j] === 1 && !visited[j]) {
-        visited[j] = true
-        dfs(j)
-      }
-    }
-  }
-  let visited = {}
-  for (let i = 0; i < n; i++) {
-    if (!visited[i]) {
-      dfs(i)
-      count++
-    }
-  }
-  return count
-}
-
-var findCircleNum = function (M) {
-  let n = M.length
   if (n == 0) return 0
   let count = 0
   let dfs = (i) => {
@@ -140,3 +51,7 @@ var findCircleNum = function (M) {
   }
   return count
 }
+
+console.log(findCircleNum([[1, 1, 0],
+[1, 1, 0],
+[0, 0, 1]]))
