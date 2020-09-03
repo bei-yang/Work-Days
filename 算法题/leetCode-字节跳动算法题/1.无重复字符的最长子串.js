681d6973c688ae202d01ef0d545e91671872ad5f
bei-yang <979623625@qq.com>
2020-09-04 07:03:20 +0800

    无重复字符长度

--- 算法题/leetCode-字节跳动算法题/1.无重复字符的最长子串.js
+++ 算法题/leetCode-字节跳动算法题/1.无重复字符的最长子串.js
@@ -93,3 +93,38 @@ const lengthOfLongestSubstring = function (s) {
   }
   return res
 }
+
+const lengthof = function (s) {
+  let num = 0
+  let res = 0
+  let m = ''
+  for (let n of s) {
+    if (m.indexOf(n) === -1) {
+      m += n
+      num++
+      res = Math.max(num, res)
+    } else {
+      m += n
+      m = m.slice(m.indexOf(n) + 1)
+      num = m.length
+    }
+  }
+  return res
+}
+
+const lengthof = function (s) {
+  let num = 0
+  let res = 0
+  let m = ''
+  for (let n of s) {
+    if (m.indexOf(n) === -1) {
+      m += n
+      num++
+      res = Math.max(num, res)
+    } else {
+      m += n
+      m = m.slice(m.indexOf(n) + 1)
+      num = m.length
+    }
+  }
+}
